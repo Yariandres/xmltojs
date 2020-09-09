197c9a04f2c5ff700ef1bf3c3f78175e453a2206
Yari Andres <yari.andres@gmail.com>
2020-09-09 21:05:20 +0200

    adding files

---
+++ index.js
@@ -0,0 +1,11 @@
+const xml2js = require('xml2js');
+const fs = require('fs');
+
+
+fs.readFile(__dirname + '/uploads/test.xml', 'utf8', function(err, data) {
+  if (err) throw new Error(err);
+  console.log(typeof data);
+  return xml = data;
+});
+
+
