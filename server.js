fbe1b0fb62e257cac799d46a7ac4750abf35e56f
Yari Andres <yari.andres@gmail.com>
2020-09-14 23:59:13 +0200

    adding client

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
+
+// Init middleware
+app.use(express.json({ extended: false }));
 
 app.use('/static', express.static(path.join(__dirname, 'static')));
 
