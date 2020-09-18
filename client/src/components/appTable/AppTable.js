83be5e27da45f54d9f81c794ea632aa772f23de8
Yari Andres <yari.andres@gmail.com>
2020-09-18 19:41:34 +0200

    adding table item component

--- client/src/components/appTable/AppTable.js
+++ client/src/components/appTable/AppTable.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Container, Button, Table, Form, Row, Col } from 'react-bootstrap';
+
 import TableNav from '../tableNav/TableNav';
+import TableItems from '../tableItems/TableItems';
 
 const AppTable = () => {
+
   return (
     <div className="appTable mt-5">
       <Container>
+
         <TableNav/>
 
         <Table striped bordered hover variant="dark">
@@ -17,24 +21,9 @@ const AppTable = () => {
               <th>Username</th>
             </tr>
           </thead>
+
           <tbody>
-            <tr>
-              <td>1</td>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td colSpan="2">Larry the Bird</td>
-              <td>@twitter</td>
-            </tr>
+            <TableItems/>
           </tbody>
         </Table>
       </Container>      
