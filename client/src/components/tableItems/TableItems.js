a29cf2c57b79119c6f81feb8862106da9991fcfa
Yari Andres <yari.andres@gmail.com>
2020-09-19 20:49:38 +0200

    adding table items

--- client/src/components/tableItems/TableItems.js
+++ client/src/components/tableItems/TableItems.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 const TableItems = () => {
-  return (    
-    <tr>
-      <td>1</td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
+  return (
+    <Fragment>
+      <tr>
+        <td>1</td>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>John</td>
+        <td>Doe</td>
+        <td>@mdo</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>Peter</td>
+        <td>Anders</td>
+        <td>@mdo</td>
+      </tr>
+      <tr>
+        <td>4</td>
+        <td>Anna</td>
+        <td>Smith</td>
+        <td>@mdo</td>
+      </tr>
+    </Fragment>    
   )
 }
 
