98cdc05b699eb79f4b880a1dd87ca9ee7936a485
Yari Andres <yari.andres@gmail.com>
2020-09-20 21:32:20 +0200

    adding files

--- client/src/components/appTable/AppTable.js
+++ client/src/components/appTable/AppTable.js
@@ -1,29 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Button, Table, Form, Row, Col } from 'react-bootstrap';
 
 import TableNav from '../tableNav/TableNav';
 import TableItems from '../tableItems/TableItems';
 
+const fetchtabledata = async () => fetch('http://localhost:8080/api/xml')
+.then(res => res.json());
+
 const AppTable = () => {
+
+  const [ tableData, setTableData ] = useState([]);
+
+  useEffect(() => {
+    fetchtabledata().then(data => setTableData(data));
+  }, []);
 
   return (
     <div className="appTable mt-5">
       <Container>
-
         <TableNav/>
 
         <Table striped bordered hover variant="dark">
           <thead>
             <tr>
-              <th>#</th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Username</th>
+              <th>Book</th>
+              <th>ID</th>
+              <th>Author</th>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Publish_Date</th>
+              <th>Description</th>
             </tr>
           </thead>
 
           <tbody>
-            <TableItems/>
+            <TableItems tableData={tableData}/>
           </tbody>
         </Table>
       </Container>      
--- routes/xml.js
+++ routes/xml.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router();
+const fs = require('fs');
 
 const xml2js = require('xml2js');
-const fs = require('fs');
+
 router.get('/xml', (req, res) => {
   fs.readFile(process.cwd() + '/uploads/test.xml', 'utf8', function(err, data) {
     if (err) throw new Error(err);    
@@ -10,11 +11,13 @@ router.get('/xml', (req, res) => {
     /**
      * @param converts xml to json object
      */
+
     xml2js.parseString(data, (err, result) => {
       if (err) throw new Error(err);
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
       res.send(result);
-    })
-    
+    });    
   });
 });
 module.exports = router;
